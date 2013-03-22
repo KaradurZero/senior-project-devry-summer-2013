0648f5a323a935daed12616f3c15c33e126ef44b
Shmelnick <Shmelnick@db5827b0-7c33-06ec-7b16-198cbf0962b0>
2013-03-22 12:30:47 +0000

    Added new script GamepadController in Assets to enable gamepad controls tweaked Vehicle collisions so that they are now physics based InputManager.asset has changed to reflect new gamepad controls

--- Trakkart/Assets/Script/PlayerControl.cs
+++ Trakkart/Assets/Script/PlayerControl.cs
@@ -8,54 +8,37 @@ public class PlayerControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		m_maxDrag = 0.5f ;
+		m_maxDrag = 1f ;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		// Amount to Move
-		float MoveRotate = player.stat.GetHandling() * Time.deltaTime;
-		float horMovement = Input.GetAxisRaw("Horizontal");
-		float vertMovement = Input.GetAxisRaw("Vertical");
+		float horMovement = Input.GetAxis("Horizontal");
+		float vertMovement = Input.GetAxis("Vertical");
 		Vector3 moveDirection= new Vector3 (horMovement, 0, vertMovement);
+		
 		rigidbody.drag = Mathf.Lerp(m_maxDrag, 0, moveDirection.magnitude);
 		 
-		 
 		// Move the player
-		//transform.Translate(Vector3.forward * player.stat.GetSpeed() * Time.deltaTime);
-		//transform.Rotate(Vector3.up * player.stat.GetHandling() * Time.deltaTime);
-		
-		if(Input.anyKey){	
-			
-			if (moveDirection != Vector3.zero){
-				Quaternion newRotation = Quaternion.LookRotation(moveDirection);
-				transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, MoveRotate);
-				rigidbody.AddForce(moveDirection * player.stat.GetCurrentSpeed());
-			}
-			
-			if(Input.GetKey(KeyCode.D)){
-				rigidbody.AddForce(moveDirection * player.stat.GetAccel()) ;
-			}
-			if(Input.GetKey(KeyCode.A)){
-				rigidbody.AddForce(moveDirection * player.stat.GetAccel()) ;
-			}
-			if(Input.GetKey(KeyCode.W)){
-				rigidbody.AddForce(moveDirection * player.stat.GetAccel()) ;
-			}
-			if(Input.GetKey(KeyCode.S)){
-				rigidbody.AddForce(moveDirection * player.stat.GetAccel()) ;
-			}
-			
-			if(Input.GetKey (KeyCode.Q)){
-				rigidbody.drag = 2f ;	
-			}
-			else{
-				rigidbody.drag = 0.5f ;	
-			}
-			
-			//if( m_orientationDiff <= 1f )
-				
+		if (moveDirection != Vector3.zero){
+			playerRotation(moveDirection) ;
 		}
+		
+		if(Input.GetKey(KeyCode.D)|| Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)){
+			rigidbody.AddForce(moveDirection * player.stat.GetAccel()) ;
+		}
+			
+		if(Input.GetKey(KeyCode.Q)){
+			rigidbody.drag = 1.5f ;	
+		}
+	}
+	
+	public void playerRotation( Vector3 direction ) {
+		float MoveRotate = player.stat.GetHandling() * Time.deltaTime;
+		Quaternion newRotation = Quaternion.LookRotation(direction);
+		transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, MoveRotate);
+		rigidbody.AddForce(direction * player.stat.GetCurrentSpeed() );	
 	}
 }
