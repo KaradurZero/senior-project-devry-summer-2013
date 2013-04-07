18d2f93d092c93241840bb37a3b28bc182d9bfed
Shmelnick <Shmelnick@db5827b0-7c33-06ec-7b16-198cbf0962b0>
2013-04-07 05:04:07 +0000

    Fixed gunfire for MACHINE_GUN types. SHOTGUN/BIG_GUN type gunfire still WIP Added "Freeze" tag and collision detection with vehicle. controls for player locked if vehicle is frozen and stays on current speed for a short time. Added AddForce function to Vehicle so that the slip coefficient from oil slicks stay inside its script. Added isFrozen function to Vehicle for control scripts to check each frame.

--- trunk/Trakkart/Assets/Script/GamepadController.cs
+++ trunk/Trakkart/Assets/Script/GamepadController.cs
@@ -13,43 +13,46 @@ public class GamepadController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float analogHorMovement = Input.GetAxis("HorizontalJ");
-		float analogVertMovement = Input.GetAxis("VerticalJ");
-		float weaponHoriz = Input.GetAxis("Weapon_Horizontal");
-		float weaponVert = Input.GetAxis("Weapon_Vertical");
-		Vector3 analogMoveDirection= new Vector3 (analogHorMovement, 0, analogVertMovement);
-		Vector3 aimDirection= new Vector3 (weaponHoriz, 0, weaponVert);
-		
-		float angle = Mathf.Atan2(weaponHoriz, weaponVert) * Mathf.Rad2Deg ;
-		
-		rigidbody.drag = Mathf.Lerp(m_maxDrag, 0, analogMoveDirection.magnitude);
-		
-		if (analogMoveDirection != Vector3.zero){
-			float MoveRotate = player.stat.GetHandling() * Time.deltaTime;
-			Quaternion newRotation = Quaternion.LookRotation(analogMoveDirection);
-			transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, MoveRotate);
-			rigidbody.AddForce(analogMoveDirection * player.stat.GetCurrentSpeed() );	
-		}
-		if (aimDirection != Vector3.zero){
-			player.weapon.Rotate( aimDirection ) ;
-		}
-		
-		rigidbody.AddForce(analogMoveDirection * player.stat.GetAccel()) ;
-		//player.weapon.Shoot(aimDirection) ;
-		
-		if( weaponHoriz != 0 || weaponVert != 0 )
-			player.weapon.Shoot( ) ;
-		
-		if( Input.GetKey (KeyCode.Joystick1Button4) ) {
-			rigidbody.drag = 2f ;
-		}
-		
-		if( Input.GetKey(KeyCode.Joystick1Button5) ) {
-			player.BoostVehicle( 0.05f ) ;
-			player.RaiseTemperaturePerSecond( 20f ) ;
-		}
-		else{
-			player.TurnOffTempPerSecond( ) ;
+		if( !player.isFrozen() ) {
+			float analogHorMovement = Input.GetAxis("HorizontalJ");
+			float analogVertMovement = Input.GetAxis("VerticalJ");
+			float weaponHoriz = Input.GetAxis("Weapon_Horizontal");
+			float weaponVert = Input.GetAxis("Weapon_Vertical");
+			Vector3 analogMoveDirection= new Vector3 (analogHorMovement, 0, analogVertMovement);
+			Vector3 aimDirection= new Vector3 (weaponHoriz, 0, weaponVert);
+			
+			float angle = Mathf.Atan2(weaponHoriz, weaponVert) * Mathf.Rad2Deg ;
+			
+			player.SetDrag(Mathf.Lerp(m_maxDrag, 0, analogMoveDirection.magnitude)) ;
+			
+			
+			if (analogMoveDirection != Vector3.zero){
+				float MoveRotate = player.stat.GetHandling() * Time.deltaTime;
+				Quaternion newRotation = Quaternion.LookRotation(analogMoveDirection);
+				transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, MoveRotate);
+				player.AddForce(analogMoveDirection, player.stat.GetCurrentSpeed());	
+			}
+			if (aimDirection != Vector3.zero){
+				player.weapon.Rotate( aimDirection ) ;
+			}
+			
+			player.AddForce(analogMoveDirection,  player.stat.GetAccel()) ;
+			//player.weapon.Shoot(aimDirection) ;
+			
+			if( weaponHoriz != 0 || weaponVert != 0 )
+				player.weapon.Shoot( ) ;
+			
+			if( Input.GetKey(KeyCode.Joystick1Button4) ) {
+				player.SetDrag( 2f ) ;
+			}
+			
+			if( Input.GetKey(KeyCode.Joystick1Button5) ) {
+				player.BoostVehicle( 0.05f ) ;
+				player.RaiseTemperaturePerSecond( 20f ) ;
+			}
+			else{
+				player.TurnOffTempPerSecond( ) ;
+			}
 		}
 	}
 }
