f1ff585b09425fb6bedcb732db60b1f8b294cd8d
heyjoelang <heyjoelang@db5827b0-7c33-06ec-7b16-198cbf0962b0>
2013-05-08 21:43:46 +0000

    modified menu, started work on point gate

--- TrakKartAlpha/Assets/Scripts/AIDriver.cs
+++ TrakKartAlpha/Assets/Scripts/AIDriver.cs
@@ -1,27 +1,25 @@
 using UnityEngine;
 using System.Collections;
-
+/*
+ * Used in conjuntion with the Vehical script.
+ * Tells the AI drivers how they should move.
+ */ 
 public class AIDriver: MonoBehaviour {
-	waypointManager myWaypoints;
-	CheckpointManagerLevel1 myCheckpoints;
-	CarStat myStats;
-	public Vector3 myTargetPos;
-	public float mySpeed;
+	CheckpointManagerLevel1 myCheckpoints;		//personal list of places to go
+	CarStat myStats;							//accel, weight, luck, ect.
+	public Vector3 myTargetPos;					//where the driver is headed
+	public float mySpeed;						//how fast the vehicle will go
 	
-	public Vehicle player ;
-	private float m_maxDrag ;
-	Vector3 pointHit;
-	bool isAlive, firstFrameDead;
-	Vector3 respawnPoint;
+	public Vehicle player ;						//every AI driver needs to know the player
+	private float m_maxDrag ;					//speed of natural decel
+	bool isAlive, firstFrameDead;				//used for respawn
+	Vector3 respawnPoint;						//where driver will respawn
 	
 	void Start () {
 		isAlive = true;
 		m_maxDrag = .5f ;
-		myWaypoints = gameObject.GetComponent<waypointManager>();
 		myCheckpoints = gameObject.GetComponent<CheckpointManagerLevel1>();
 		myStats = gameObject.GetComponent<CarStat>();
-		if(myWaypoints != null)
-			myTargetPos = myWaypoints.GetCurrWaypointPos();
 		if(myCheckpoints != null)
 			myTargetPos = myCheckpoints.GetCurrCheckpointPos();
 	}
@@ -35,15 +33,16 @@ public class AIDriver: MonoBehaviour {
 	{
 		isAlive = true;
 	}
-	// Update is called once per frame
 	void Update () {
+		//if driver is dead, do not update anything
 		if(isAlive)
 		{
+			//if driver is frozen, do not update anything
 			if( !player.isFrozen() ) {
 				// Amount to Move
 				float MoveRotate = player.stat.GetHandling() * Time.deltaTime;
-				float horMovement;// = Input.GetAxisRaw("Horizontal");
-				float vertMovement;// = Input.GetAxisRaw("Vertical");
+				float horMovement;
+				float vertMovement;
 				float xDirection = myTargetPos.x - transform.position.x;
 				float zDirection = myTargetPos.z - transform.position.z;
 				
@@ -51,34 +50,27 @@ public class AIDriver: MonoBehaviour {
 				vertMovement = 0;
 				if(myTargetPos != null)
 				{
-					if(xDirection > 0)
+					if(xDirection > 1)
 						horMovement = 1f;
-					else if(xDirection < 0)
+					else if(xDirection < -1)
 						horMovement = -1f;
 					else 
 						horMovement = 0;
-					if(zDirection > 0)
+					if(zDirection > 1)
 						vertMovement = 1f;
-					else if(zDirection < 0)
+					else if(zDirection < -1)
 						vertMovement = -1f;
 					else
 						vertMovement = 0;
 				}
 				Vector3 moveDirection= new Vector3 (horMovement, 0, vertMovement);
-				//rigidbody.drag = Mathf.Lerp(m_maxDrag, 0, moveDirection.magnitude);
 				player.SetDrag(Mathf.Lerp(m_maxDrag, 0, moveDirection.magnitude)) ;
 				
 				if( !player.stat.isOverheated() ) {
 					if(Vector3.Distance(transform.position, myTargetPos) <= Random.Range(1f,5f))
 					{
-						if(myWaypoints != null)
-						{
-							myWaypoints.NextWaypoint();
-							myTargetPos = myWaypoints.GetCurrWaypointPos();
-						}
 						if(myCheckpoints != null)
 						{
-							//myCheckpoints.NextCheckpoint();
 							myTargetPos = myCheckpoints.GetCurrCheckpointPos();
 						}
 					}
@@ -88,18 +80,12 @@ public class AIDriver: MonoBehaviour {
 						if (moveDirection != Vector3.zero){
 							Quaternion newRotation = Quaternion.LookRotation(moveDirection);
 							transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, MoveRotate);
-							//rigidbody.AddForce(moveDirection * player.stat.GetMaxVelocity());//GetCurrentSpeed());
 							player.AddForce(moveDirection, player.stat.GetCurrentSpeed());	
 						}
 						rigidbody.drag = 0.5f ;
 					}
 					//transform.position = new Vector3(transform.position.x, 1.0f, transform.position.z);
 					player.AddForce(moveDirection,  player.stat.GetAccel()) ;
-//					if(Vector3.Distance(myWaypoints.GetCurrWaypointPos(), transform.position) >= 1f)
-//					{
-//						player.BoostVehicle( 0.5f ) ;
-						//player.RaiseTemperaturePerSecond( 10f ) ;
-//					}
 				}
 			}
 			else
@@ -110,8 +96,6 @@ public class AIDriver: MonoBehaviour {
 			if(firstFrameDead)
 			{
 				firstFrameDead = false;
-				if(GetComponent<waypointManager>() != null)
-					respawnPoint = GetComponent<waypointManager>().GetLastWaypointPos();
 				if(GetComponent<CheckpointManagerLevel1>() != null)
 					respawnPoint = GetComponent<CheckpointManagerLevel1>().GetLastCheckpointPos();
 			}
