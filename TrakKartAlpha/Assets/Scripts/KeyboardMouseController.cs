bfe2eca55ebc883924d9ebbcb1ef66047b158fa1
Shmelnick <Shmelnick@db5827b0-7c33-06ec-7b16-198cbf0962b0>
2013-05-20 21:48:20 +0000

    Vehicle - Fixed grass, boost pad, and oil slick to be used as triggers instead of giving it their own rigidbody Fixed issue with oil slick not working. Added code to turn of slipping when vehicle is no longer affected.

--- TrakKartAlpha/Assets/Scripts/KeyboardMouseController.cs
+++ TrakKartAlpha/Assets/Scripts/KeyboardMouseController.cs
@@ -24,58 +24,60 @@ public class KeyboardMouseController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(!player.isFrozen()) {
-			float horMovement = Input.GetAxis("Horizontal");
-			float vertMovement = Input.GetAxis("Vertical");
-			Vector3 moveDirection= new Vector3 (horMovement, 0, vertMovement);
-			
-			player.SetDrag(Mathf.Lerp(m_maxDrag, 0, moveDirection.magnitude)) ;
-			
-			if( !player.stat.isOverheated() ) {
-				if (moveDirection != Vector3.zero){
-					float MoveRotate = player.stat.GetHandling() * Time.deltaTime;
-					Quaternion newRotation = Quaternion.LookRotation(moveDirection);
-					transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, MoveRotate);
-					player.AddForce(moveDirection, player.stat.GetCurrentSpeed());	
+		if( player.amAlive() ) {
+			if(!player.isFrozen()) {
+				float horMovement = Input.GetAxis("Horizontal");
+				float vertMovement = Input.GetAxis("Vertical");
+				Vector3 moveDirection= new Vector3 (horMovement, 0, vertMovement);
+				
+				player.SetDrag(Mathf.Lerp(m_maxDrag, 0, moveDirection.magnitude)) ;
+				
+				if( !player.stat.isOverheated() ) {
+					if (moveDirection != Vector3.zero){
+						float MoveRotate = player.stat.GetHandling() * Time.deltaTime;
+						Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+						transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, MoveRotate);
+						player.AddForce(moveDirection, player.stat.GetCurrentSpeed());	
+					}
+					
+					if(Input.GetKeyDown(KeyCode.Mouse2) && !m_swapButtonDown) { 
+					m_swapButtonDown = true ;
+					myGunShieldRot.swapGunShield() ;	
+					}
+					else if( m_swapButtonDown ) {
+						m_swapButtonDown = false ;
+					}
+					
+					myGunShieldRot.updateRotationToMouse();
+					
+					player.AddForce(moveDirection,  player.stat.GetAccel()) ;
+					//player.weapon.Shoot(aimDirection) ;
+					
+					if(Input.GetKey(KeyCode.Mouse0) && myGunShieldRot.isGunEnabled() && myWeapon.CanShoot()) {
+						myWeapon.fireBullet() ;
+						player.RaiseTemp( true ) ;
+						}
+					
+					if(Input.GetKey(KeyCode.Space)) {
+						if( player.myPowerup.item != 0 )
+							myPowerup.UseItem() ;
+						}
+					
+					if( Input.GetKey(KeyCode.Q) ) {
+						player.SetDrag( 2f ) ;
+						}
+							
+					if( Input.GetKey(KeyCode.Mouse1) ) {
+						player.BoostVehicle( 0.05f ) ;
+						}
+					else{
+						player.TurnOffTempPerSecond( ) ;
+						}
+					}
 				}
-				
-				if(Input.GetKeyDown(KeyCode.Mouse2) && !m_swapButtonDown) { 
-				m_swapButtonDown = true ;
-				myGunShieldRot.swapGunShield() ;	
-				}
-				else if( m_swapButtonDown ) {
-					m_swapButtonDown = false ;
-				}
-				
-				myGunShieldRot.updateRotationToMouse();
-				
-				player.AddForce(moveDirection,  player.stat.GetAccel()) ;
-				//player.weapon.Shoot(aimDirection) ;
-				
-				if(Input.GetKey(KeyCode.Mouse0) && myGunShieldRot.isGunEnabled() && myWeapon.CanShoot()) {
-					myWeapon.fireBullet() ;
-					player.RaiseTemp( true ) ;
-					}
-				
-				if(Input.GetKey(KeyCode.Space)) {
-					if( player.myPowerup.item != 0 )
-						myPowerup.UseItem() ;
-					}
-				
-				if( Input.GetKey(KeyCode.Q) ) {
-					player.SetDrag( 2f ) ;
-					}
-						
-				if( Input.GetKey(KeyCode.Mouse1) ) {
-					player.BoostVehicle( 0.05f ) ;
-					}
-				else{
-					player.TurnOffTempPerSecond( ) ;
-					}
-				}
+			else {
+				player.SetDrag(0f) ;
 			}
-		else {
-			player.SetDrag(0f) ;
 		}
 		
 		//Debug.Log(player.stat.GetCurrTemp()) ;
