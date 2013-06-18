4a090dda87247f9c4017b3dc73a311c05321ed5d
Shmelnick <Shmelnick@db5827b0-7c33-06ec-7b16-198cbf0962b0>
2013-06-18 14:18:49 +0000

    -quick fixes to control scripts -frozen vehicles now turn blue when frozen -overheated vehicles turn red when overheated

--- trunk/TrakKartAlpha/Assets/Scripts/GamepadController.cs
+++ trunk/TrakKartAlpha/Assets/Scripts/GamepadController.cs
@@ -26,84 +26,87 @@ public class GamepadController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//is player alive?
-		if( player.amAlive() ) {
-			//lock controls when player is frozen/overheated
-			if(!player.isFrozen()) {
-				if( !player.stat.isOverheated() ) {
-					//input poll
-					float analogHorMovement = Input.GetAxis("HorizontalJ");
-					float analogVertMovement = Input.GetAxis("VerticalJ");
-					float weaponHoriz = Input.GetAxis("Weapon_Horizontal");
-					float weaponVert = Input.GetAxis("Weapon_Vertical");
-					Vector3 analogMoveDirection= new Vector3 (analogHorMovement, 0, analogVertMovement);
-					Vector3 aimDirection= new Vector3 (weaponHoriz, 0, weaponVert);
-					
-					//set drag
-					player.SetDrag(Mathf.Lerp(m_maxDrag, 0, analogMoveDirection.magnitude)) ;
-				
-					
-						//rotate vehicle based on player direction
-						if (analogMoveDirection != Vector3.zero){
-							float MoveRotate = player.stat.GetHandling() * Time.deltaTime;
-							Quaternion newRotation = Quaternion.LookRotation(analogMoveDirection);
-							transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, MoveRotate);
-							//add force for smoother turning
-							player.AddForce(analogMoveDirection, player.stat.GetCurrentSpeed());	
-						}
-					
-						//rotate gun/shield based on right thumbstick direction
-						if (aimDirection != Vector3.zero) {
-							myGunShieldRot.updateRotationToVec3(aimDirection) ;
-						}
-					
-						//move vehicle
-						player.AddForce(analogMoveDirection,  player.stat.GetAccel()) ;
-						//player.weapon.Shoot(aimDirection) ;
-					
-					//*****************************************Player Input********************************************************//
+		if( Input.GetJoystickNames()[0] != "Controller (Gamepad for Xbox 360)" ) {
+			//is player alive?
+			if( player.amAlive() ) {
+				//lock controls when player is frozen/overheated
+				if(!player.isFrozen()) {
+					if( !player.stat.isOverheated() ) {
+						//input poll
+						float analogHorMovement = Input.GetAxisRaw("HorizontalJ");
+						float analogVertMovement = Input.GetAxisRaw("VerticalJ");
+						float weaponHoriz = Input.GetAxis("Weapon_Horizontal");
+						float weaponVert = Input.GetAxis("Weapon_Vertical");
+						Vector3 analogMoveDirection= new Vector3 (analogHorMovement, 0, analogVertMovement);
+						Vector3 aimDirection= new Vector3 (weaponHoriz, 0, weaponVert);
+						//Vector3 aimDirection= new Vector3 (-weaponVert, 0, -weaponHoriz);
 						
-						//fire weapon if thumbstick is moved
-						if( (weaponHoriz != 0 || weaponVert != 0) && myGunShieldRot.isGunEnabled() && myWeapon.CanShoot()) {
-							myWeapon.fireBullet() ;
-							player.RaiseTemp( true ) ;
+						//set drag
+						player.SetDrag(Mathf.Lerp(m_maxDrag, 0, analogMoveDirection.magnitude)) ;
+					
+						
+							//rotate vehicle based on player direction
+							if (analogMoveDirection != Vector3.zero){
+								float MoveRotate = player.stat.GetHandling() * Time.deltaTime;
+								Quaternion newRotation = Quaternion.LookRotation(analogMoveDirection);
+								transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, MoveRotate);
+								//add force for smoother turning
+								player.AddForce(analogMoveDirection, player.stat.GetCurrentSpeed());	
+							}
+						
+							//rotate gun/shield based on right thumbstick direction
+							if (aimDirection != Vector3.zero) {
+								myGunShieldRot.updateRotationToVec3(aimDirection) ;
+							}
+						
+							//move vehicle
+							player.AddForce(analogMoveDirection,  player.stat.GetAccel()) ;
+							//player.weapon.Shoot(aimDirection) ;
+						
+						//*****************************************Player Input********************************************************//
 							
-						}
+							//fire weapon if thumbstick is moved
+							if( (weaponHoriz != 0 || weaponVert != 0) && myGunShieldRot.isGunEnabled() && myWeapon.CanShoot()) {
+								myWeapon.fireBullet() ;
+								player.RaiseTemp( true ) ;
+								
+							}
+							
+							//brake
+							if( Input.GetKey(KeyCode.Joystick1Button4) /*|| Input.GetKey(KeyCode.Joystick2Button4)*/) {
+								player.SetDrag( 3f ) ;
+								Debug.Log ("BRAKE");
+							}
+							
+							//swap gun/shield
+							if(Input.GetKeyDown(KeyCode.Joystick1Button0) && !m_swapButtonDown){
+								m_swapButtonDown = true ;
+								myGunShieldRot.swapGunShield() ;	
+							}
+							else if( m_swapButtonDown ) {
+								m_swapButtonDown = false ;
+							}
+							
+							//boost vehicle
+							if( Input.GetKey(KeyCode.Joystick1Button5)) {
+								player.BoostVehicle( 0.05f, false ) ;
+							}
+							else{
+								player.TurnOffTempPerSecond( ) ;
+							}
 						
-						//brake
-						if( Input.GetKey(KeyCode.Joystick1Button4) /*|| Input.GetKey(KeyCode.Joystick2Button4)*/) {
-							player.SetDrag( 3f ) ;
-							Debug.Log ("BRAKE");
+							//use powerup
+							if( Input.GetKeyDown(KeyCode.Joystick1Button1)) {
+							if( player.myPowerup.item != 0 )
+								player.FirePowerUp() ;
 						}
-						
-						//swap gun/shield
-						if(Input.GetKeyDown(KeyCode.Joystick1Button0) && !m_swapButtonDown){
-							m_swapButtonDown = true ;
-							myGunShieldRot.swapGunShield() ;	
-						}
-						else if( m_swapButtonDown ) {
-							m_swapButtonDown = false ;
-						}
-						
-						//boost vehicle
-						if( Input.GetKey(KeyCode.Joystick1Button5)) {
-							player.BoostVehicle( 0.05f, false ) ;
-						}
-						else{
-							player.TurnOffTempPerSecond( ) ;
-						}
-					
-						//use powerup
-						if( Input.GetKeyDown(KeyCode.Joystick1Button1)) {
-						if( player.myPowerup.item != 0 )
-							player.FirePowerUp() ;
 					}
+					else
+						player.SetDrag(m_maxDrag) ; //vehicle drags when player doesn't move or overheated
 				}
-				else
-					player.SetDrag(m_maxDrag) ; //vehicle drags when player doesn't move or overheated
-			}
-			else {
-				player.SetDrag(0f) ; //vehicle slips aroung when frozen
+				else {
+					player.SetDrag(0f) ; //vehicle slips aroung when frozen
+				}
 			}
 		}
 	}
